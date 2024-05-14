3803cd2cfd3dc4f26fb6d84f2ffb99bd5792d8e3
Laithe Marshall <laithe.marshall@hivemedia.com>
2024-05-14 16:41:36 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Start with 16x16 grid of square divs
+// Use flexbox for alignment
+// Create hover effect that changes the color of the div on mouseover
+//Add button at the top that creates a prompt to prompt the user to input an int to be the length of the square min 16x16 max 100x100
