f4ddbe35cfd37eed276eaa455a824b656fff5d49
Laithe Marshall <loaf1000@gmail.com>
2024-05-21 13:57:52 -0700

    Custom Grid size

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // Start with 16x16 grid of square divs
 // Use flexbox for alignment
 // Create hover effect that changes the color of the div on mouseover
-//Add button at the top that creates a prompt to prompt the user to input an int to be the length of the square min 16x16 max 100x100
+//Add button at the top that creates a prompt to prompt the user to input an int to be the length of the square min 2x2 max 100x100
 const container = document.querySelector(".container");
+// 1024px x 1024px
+const containerLength = 1024;
+const buttonChanger = document.querySelector("#changer");
 
 function createGrid(gridLength) {
+    const squareLength = getPixelDimenions(gridLength);
 
-    //TODO: handle pixel width and height
     for (let i = 0; i < gridLength * gridLength; i++)
         {
             let div = document.createElement("div");
             div.setAttribute("class", "pixel");
             div.setAttribute("id", "pixel_" + i);
-            //div.setAttribute("style", "border: 1px solid black; color: white; height: 64px; width: 64px");
+            div.setAttribute("style", "height: " + squareLength + "; width: "+ squareLength);
 
             div.addEventListener("mouseover", () => {
                 div.classList.toggle("rabble");
@@ -27,5 +30,37 @@ function createGrid(gridLength) {
 
         }
 }
+
+function removeGrid () {
+    while (container.firstChild)
+        {
+            container.removeChild(container.firstChild);
+        }
+
+};
+
+function getNumberOfSquares () {
+    //
+   let sign = Number(prompt("Choose a number squares per side for a new grid Min:2 Max:100 Default:16"))
+
+   if (sign < 2 || sign > 100 || isNaN(sign))
+    {
+        sign = 16;
+    }
+   return sign;
+};
+
+function getPixelDimenions (numberOfSquares) {
+    const settingTemplate = "px";
+    let estimate = (containerLength / numberOfSquares) - 2;
+
+    return estimate + settingTemplate;
+}
+
+buttonChanger.addEventListener("click", () => {
+    const numberOfSquares = getNumberOfSquares();
+    removeGrid();
+    createGrid(numberOfSquares);
+});
 
 createGrid(16);
